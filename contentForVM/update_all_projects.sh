966561be226bf24dce4e4e09548865333d9f6d0c
Jonas Danielsson <jonas@bitcraze.io>
2021-04-08 07:43:49 +0200

    update_all_projects: Explictly reinstall lib and client
    The order is important right now, we need to install the lib before
    the client in order to get the right version of the dependencies.

--- contentForVM/update_all_projects.sh
+++ contentForVM/update_all_projects.sh
@@ -11,17 +11,16 @@ do
 		git submodule init
 		git submodule update --recursive
 
-		#
-		# In order to make sure the user has an up-to-date install of the
-		# python lib and client, with all deps, we make sure to re-install it
-		# from the source in git.
-		#
-		if [ -f setup.py ]; then  # true if this is a python project
-			pip3 install --user --upgrade --force-reinstall -e .
-		fi
-
 		cd ..
 	fi
 done
+
+#
+# In order to make sure the user has an up-to-date install of the
+# python lib and client, with all deps, we make sure to re-install it
+# from the source in git.
+#
+pip3 install --user --force-reinstall -e /home/bitcraze/projects/crazyflie-lib-python
+pip3 install --user --force-reinstall -e /home/bitcraze/projects/crazyflie-clients-python
 
 read -p "Press any key to exit" -n1 -s
