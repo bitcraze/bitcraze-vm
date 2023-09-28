378c2555877d69f01233b70de1c808ae343c96a6
Kristoffer Richardsson <kristoffer@bitcraze.io>
2023-09-28 16:39:53 +0200

    Install VSCode from snap

--- provision-sudo.sh
+++ provision-sudo.sh
@@ -56,14 +56,7 @@ chown bitcraze:bitcraze -R projects
 ln -s ~/projects ~/Desktop/projects
 
 # Install VSCode
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
-
-apt install apt-transport-https
-apt update
-apt install code # or code-insiders
+snap install --classic code
 
 # Install VSCode extensions
 sudo -H -u bitcraze code --install-exteman nsion ms-python.python
